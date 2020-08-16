9877e8dcac28d8f6aef61b84bb1f2db73af9c7b4
amandujano <a.mandujanogarcia@gmail.com>
2020-08-16 16:23:10 -0500

    update: redesign header. new styling

--- src/components/header.es.js
+++ src/components/header.es.js
@@ -6,30 +6,50 @@ import { Trans } from "@lingui/macro";
 import { I18nProvider } from "@lingui/react";
 import catalogEN from "../locales/en/messages";
 import catalogES from "../locales/es/messages";
+import Image from "gatsby-image";
 
 const NavLink = styled(Link)`
-  color: #eeeeee;
-  font-size: 1.2rem;
+  font-size: 1rem;
   font-weight: ${props => props.fontWeight || "normal"};
   line-height: 1;
-  margin: 0 0.5rem 0 0;
+  margin: 0.5rem 1rem 0 0;
   padding: 0.25rem;
   text-decoration: none;
-  color: #ffffff;
-  text-shadow: 3px 5px 2px #474747;
+  color: #8ab8fe;
+  background-color: transparent;
+  border: 2px solid #8ab8fe;
+  border-radius: 6px;
+  position: relative;
+  overflow: hidden;
+  transition: all 0.5s cubic-bezier(1, 0.15, 0.34, 0.92);
+
+  &:hover {
+    box-sizing: border-box;
+    border-width: 1;
+    border-style: solid;
+    border-color: #d2d6dc;
+    color: #ffff7e;
+    background-color: #8ab8fe;
+  }
+
   &.current-page {
-    border-bottom: 2px solid #ffffff;
+    box-sizing: border-box;
+    border-width: 1;
+    border-style: solid;
+    border-color: #d2d6dc;
+    color: #ffff7e;
+    background-color: #8ab8fe;
   }
 
   &:last-of-type {
-    margin-right: 0;
+    margin-right: 10;
   }
 `;
 
 const Header = () => {
   const { image } = useStaticQuery(graphql`
     query {
-      image: file(relativePath: { eq: "air4childrenLogo1.png" }) {
+      image: file(relativePath: { eq: "logo.png" }) {
         sharp: childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid_withWebp
@@ -43,23 +63,35 @@ const Header = () => {
     <I18nProvider language="es" catalogs={{ en: catalogEN, es: catalogES }}>
       <header
         css={css`
-          background: #1f4f73;
-          border-bottom: 3px solid #b34f28;
+          background: #ffff7e;
+          border-bottom: 8px solid #8ab8fe;
           display: flex;
           justify-content: space-between;
-          padding: 0.5rem calc((100vw - 950px - 0.5rem) / 2);
+          padding: 0.5rem calc((100vw - 1300px - 0.5rem) / 2);
           height: 80px;
-          font-family: "Balsamiq Sans", cursive;
         `}
       >
-        <NavLink to="/es/" fontWeight="bold">
-          {" "}
-          <Trans>Air4Children</Trans>
-        </NavLink>
+        <Link
+          to="/en/"
+          css={css`
+            margin: 0.5rem 1rem 0 0;
+            width: 20%;
+          `}
+        >
+          <Image
+            css={css`
+              * {
+                margin-top: 0;
+              }
+            `}
+            fluid={image.sharp.fluid}
+            alt="air4children"
+          />
+        </Link>
 
         <nav
           css={css`
-            margin-top: 0;
+            margin: 0.5rem 1rem 0 0;
           `}
         >
           <NavLink to="/es/about/" activeClassName="current-page">
@@ -71,15 +103,42 @@ const Header = () => {
           <NavLink to="/es/volunteering/" activeClassName="current-page">
             <Trans>Volunteering</Trans>
           </NavLink>
-          <NavLink to="/es/donate/" activeClassName="current-page">
-            <Trans>Donate</Trans>
-          </NavLink>
           <NavLink to="/es/contact/" activeClassName="current-page">
             <Trans>Contact Us</Trans>
           </NavLink>
           <NavLink to="/en/" activeClassName="current-page">
             <Trans>en</Trans>
           </NavLink>
+        </nav>
+        <nav
+          css={css`
+            margin: 0.5rem 1rem 0 0;
+          `}
+        >
+          <Link
+            to="/en/donate/"
+            activeClassName="current-page"
+            css={css`
+              font-size: 1rem;
+              font-weight: ${props => props.fontWeight || "normal"};
+              line-height: 1;
+              margin: 1rem 1rem 1 1;
+              padding: 0.35rem;
+              text-decoration: none;
+              color: #ffff7e;
+              background-color: #665fd1;
+              position: relative;
+              overflow: hidden;
+              box-sizing: border-box;
+              border: 2px solid #8ab8fe;
+              border-radius: 6px;
+              border-width: 1;
+              border-style: solid;
+              border-color: #d2d6dc;
+            `}
+          >
+            <Trans>Donate</Trans>
+          </Link>
         </nav>
       </header>
     </I18nProvider>
