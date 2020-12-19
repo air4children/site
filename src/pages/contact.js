7f83aaed75ab8326a8d281a743d7d48ae118c5bd
amandujano <a.mandujanogarcia@gmail.com>
2020-12-19 11:37:24 -0600

    update: contact and donate page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,36 @@ import React from "react";
 import Layout from "../components/layout";
 import { Trans } from "@lingui/macro";
 import Wave from "../components/wave";
+import { Divider, Form, Input, InputNumber, Button } from 'antd';
+import Paper from '@material-ui/core/Paper';
+
+const layoutt = {
+  labelCol: {
+    span: 6,
+  },
+  wrapperCol: {
+    span: 12,
+  },
+};
+
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
+
 
 export default ({ location }) => {
+
+  const onFinish = (values) => {
+    console.log(values);
+  };
+  
   return (
     <Layout location={location.pathname}>
       <h1>
@@ -20,11 +48,69 @@ export default ({ location }) => {
 
       <p>
         <Trans>
-          Also, you can create an issue in any of the repositories in
-          https://github.com/air4children.
+          It can also be helpul to fill the following form in order to send us a mail.
         </Trans>
       </p>
-      <Wave />
+
+      <Divider />
+
+    <Form {...layoutt} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+      <Form.Item
+        name={['user', 'name']}
+        label="Name"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name={['user', 'email']}
+        label="Email"
+        rules={[
+          {
+            type: 'email',
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name={['user', 'age']}
+        label="Age"
+        rules={[
+          {
+            type: 'number',
+            min: 0,
+            max: 99,
+          },
+        ]}
+      >
+        <InputNumber />
+      </Form.Item>
+      <Form.Item name={['user', 'introduction']} label="Introduction">
+        <Input.TextArea />
+      </Form.Item>
+      <Form.Item wrapperCol={{ ...layoutt.wrapperCol, offset: 8 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+
+    <Divider />
+
+    <p>
+      <Trans>
+        Also, you can create an issue in any of the repositories in
+        https://github.com/air4children.
+      </Trans>
+    </p>
+
+
     </Layout>
   );
 };
--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,12 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/layout";
 import { Trans } from "@lingui/macro";
 import { LocalizedLink } from "gatsby-theme-i18n";
+import {
+  Form,
+  Input,
+  Button,
+  Radio,
+  Select,
+  Cascader,
+  DatePicker,
+  InputNumber,
+  TreeSelect,
+  Switch,
+  Divider
+} from 'antd';
 
-export default ({ location }) => (
-  <Layout location={location.pathname}>
-    <h1><Trans>Donate</Trans></h1>
-    <p><Trans>can donate here</Trans></p>
-    <LocalizedLink to="/">&larr; <Trans>back to home</Trans></LocalizedLink>
-  </Layout>
-);
+export default ({ location }) => {
+  const [componentSize, setComponentSize] = useState('default');
+
+  const onFormLayoutChange = ({ size }) => {
+    setComponentSize(size);
+  };
+
+  return (
+    <Layout location={location.pathname}>
+
+      <h1><Trans>Donate</Trans></h1>
+      <p><Trans>We offer fun literacy programs for kids in Xicohtzinco and volunteer opportunities for literacy lovers who want to see kids thrive. </Trans></p>
+      <LocalizedLink to="/">&larr; <Trans>back to home</Trans></LocalizedLink>
+
+
+      <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="horizontal"
+        initialValues={{
+          size: componentSize,
+        }}
+        onValuesChange={onFormLayoutChange}
+        size={componentSize}
+      >
+        <Divider orientation="left">Donate to</Divider>
+        <Form.Item label="Donate to">
+          <Select>
+            <Select.Option value="demo">where it is needed most</Select.Option>
+            <Select.Option value="uno">blocky robots</Select.Option>
+          </Select>
+        </Form.Item>
+
+        <Divider orientation="left">Donation amount</Divider>
+
+        <Form.Item
+          name="radio-button"
+          label="Radio.Button"
+          rules={[
+            {
+              required: true,
+              message: 'Please pick an item!',
+            },
+          ]}
+        >
+          <Radio.Group span="20">
+            <Radio.Button value="a">$50</Radio.Button>
+            <Radio.Button value="b">$100</Radio.Button>
+            <Radio.Button value="c">$200</Radio.Button>
+            <Radio.Button value="d">$300</Radio.Button>
+            <Radio.Button value="e">$500</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+
+        <Form.Item label="InputNumber">
+          <InputNumber />
+        </Form.Item>
+
+        <Divider orientation="left">Donation frequency</Divider>
+
+        <Form.Item label="Switch">
+          <Switch />
+        </Form.Item>
+        <Form.Item label="Button">
+          <Button>Button</Button>
+        </Form.Item>
+      </Form>
+
+
+
+
+    </Layout>
+  );
+};
